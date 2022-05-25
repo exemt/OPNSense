013f955001224ce796026baea2ac131199fe12da
BuildTools <unconfigured@null.spigotmc.org>
2022-05-25 13:48:44 +0300

    firewall: remove file_notice() in filter HA sync

---
+++ src/opnsense/mvc/app/models/Gerden/IndexController.php
@@ -0,0 +1,10 @@
+<?php
+namespace OPNsense\GerdenPing;
+class IndexController extends \OPNsense\Base\IndexController
+{
+    public function indexAction()
+    {
+        // pick the template to serve to our users.
+        $this->view->pick('OPNsense/GerdenPing/index');
+    }
+}
